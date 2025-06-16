29c4349dbb88dc9d3efedf54f082b51f18fc48e5
Claire Thomas <claire.cicle@gmail.com>
2025-06-16 11:02:06 -0600

    Firebase updates

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,6 +9,8 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Separator } from "@/components/ui/separator"
 import { useRouter } from "next/navigation"
+import {  signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "@/firebase/config";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("")
@@ -33,7 +35,37 @@ export default function LoginPage() {
   }
 
   const handleGoogleLogin = async () => {
+
+
+
     setIsLoading(true)
+
+    const provider = new GoogleAuthProvider();
+
+    // const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+
+        console.log(token, user)
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+
+        console.log(errorCode, errorMessage, email, credential)
+      });
 
     // Simulate Google login - replace with actual authentication
     try {
